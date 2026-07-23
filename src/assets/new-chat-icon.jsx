702f6f8550d4784187e61c2e69f0e61d253f5d9d
Angel <etti.joseangel@gmail.com>
2026-07-22 19:42:00 -0600

    fix: importar History en main.jsx y configurar servidor Vite en puerto 3000

--- src/assets/new-chat-icon.jsx
+++ src/assets/new-chat-icon.jsx
@@ -1,4 +1,4 @@
-function NewChatIcon() {
+export const NewChatIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -7,10 +7,10 @@ function NewChatIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-message-circle-more"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-message-circle-more"
     >
       <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
       <path d="M8 12h.01" />
@@ -18,6 +18,4 @@ function NewChatIcon() {
       <path d="M16 12h.01" />
     </svg>
   );
-}
-
-export { NewChatIcon };
+};
